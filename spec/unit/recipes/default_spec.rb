c5ce351df1d0c5b0110654141b125a2e44852a4e
James Murphy <jmurphy@spartaglobal.com>
2020-03-11 11:53:53 +0000

    initial commit

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -7,23 +7,61 @@
 require 'spec_helper'
 
 describe 'node_cookbook::default' do
-  context 'When all attributes are default, on Ubuntu 18.04' do
+  context 'When all attributes are default, on Ubuntu 16.04' do
     # for a complete list of available platforms and versions see:
     # https://github.com/chefspec/fauxhai/blob/master/PLATFORMS.md
-    platform 'ubuntu', '18.04'
+    platform 'ubuntu', '16.04'
 
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
     end
-  end
 
-  context 'When all attributes are default, on CentOS 7' do
-    # for a complete list of available platforms and versions see:
-    # https://github.com/chefspec/fauxhai/blob/master/PLATFORMS.md
-    platform 'centos', '7'
+    it 'should apt_update' do
+      expect(chef_run).to update_apt_update 'update_sources'
+    end
 
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
+    it 'should install nginx' do
+      expect(chef_run).to install_package "nginx"
+    end
+
+    it 'enables nginx' do
+      expect(chef_run).to enable_service 'nginx'
+    end
+
+    it 'start nginx' do
+      expect(chef_run).to start_service 'nginx'
+    end
+
+
+    # template test additions
+    it "should create proxy.conf" do
+      expect(chef_run).to create_template("/etc/nginx/sites-available/proxy.conf").with_variables(proxy_port: 3000)
+    end
+
+    it "should delete existing link between default" do
+      expect(chef_run).to delete_link('/etc/nginx/sites-enabled/default')
+    end
+
+    it "assert a link was created" do
+      expect(chef_run).to create_link('/etc/nginx/sites-enabled/proxy.conf').with(to: '/etc/nginx/sites-available/proxy.conf')
+    end
+
+    it "installed nodejs from recipe" do
+      expect(chef_run).to include_recipe("nodejs")
+    end
+
+    it "install pm2 via npm" do
+      expect(chef_run).to install_nodejs_npm('pm2')
     end
   end
+
+  # context 'When all attributes are default, on CentOS 7' do
+  #   # for a complete list of available platforms and versions see:
+  #   # https://github.com/chefspec/fauxhai/blob/master/PLATFORMS.md
+  #   platform 'centos', '7'
+
+  #   it 'converges successfully' do
+  #     expect { chef_run }.to_not raise_error
+  #   end
+  # end
 end
